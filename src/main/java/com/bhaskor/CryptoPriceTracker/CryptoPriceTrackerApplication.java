7b4867f8cb1cecde80241cbdacfb3845cc0860c0
Bhaskor Jyoti Sarmah <bhaskor.sarmah@privacera.com>
2022-05-21 14:18:53 +0530

    Initial Commit

---
+++ src/main/java/com/bhaskor/CryptoPriceTracker/CryptoPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bhaskor.CryptoPriceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPriceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhaskor/CryptoPriceTracker/CryptoPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskor.CryptoPriceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
